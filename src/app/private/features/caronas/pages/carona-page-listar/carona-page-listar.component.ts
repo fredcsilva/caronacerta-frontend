17b3ec9775739dc52bb5a74471c30e50d8ee4fc0
Fred Carvalho <fredcsilva@gmail.com>
2025-10-23 20:42:28 -0300

    refactor: migrar feature caronas

---
+++ src/app/private/features/caronas/pages/carona-page-listar/carona-page-listar.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuItem } from 'primeng/api';
+import { DockModule } from 'primeng/dock';
+import { TooltipModule } from 'primeng/tooltip';
+import { RippleModule } from 'primeng/ripple';
+import { BottomNavComponent } from '../../../../layout/bottom-nav/bottom-nav.component';
+import { CardCaronaComponent, CaronaModel } from '../../../../../shared/components/card-carona/card-carona';
+
+@Component({
+  selector: 'app-listar-caronas',
+  standalone: true,
+  imports: [
+    CommonModule,
+    DockModule,
+    TooltipModule,
+    RippleModule,
+    BottomNavComponent,
+    CardCaronaComponent
+  ],
+  templateUrl: './carona-page-listar.component.html',
+  styleUrls: ['./carona-page-listar.component.css']
+})
+export class CaronaPageListarComponent implements OnInit {
+  dockItems: MenuItem[] = [];
+  caronas: CaronaModel[] = [];
+
+  ngOnInit(): void {
+    this.dockItems = [
+      { label: 'Caronas', icon: 'pi pi-car', tooltipOptions: { tooltipLabel: 'Minhas Caronas' } },
+      { label: 'Histórico', icon: 'pi pi-history', tooltipOptions: { tooltipLabel: 'Histórico' } },
+      { label: 'Nova Carona', icon: 'pi pi-plus-circle', tooltipOptions: { tooltipLabel: 'Nova Carona' }, styleClass: 'destaque' },
+      { label: 'Solicitações', icon: 'pi pi-file-check', tooltipOptions: { tooltipLabel: 'Solicitações' } },
+      { label: 'Menu', icon: 'pi pi-bars', tooltipOptions: { tooltipLabel: 'Menu' } }
+    ];
+
+    // Mock de caronas
+    this.caronas = [
+      {
+        data: '16/10',
+        hora: '08:30',
+        origem: 'Bloco A',
+        destino: 'Shopping Midway',
+        valor: 10,
+        nome: 'Carlos Souza',
+        avaliacao: 4.7,
+        bloco: 'A',
+        apto: '103'
+      },
+      {
+        data: '16/10',
+        hora: '17:00',
+        origem: 'Shopping Midway',
+        destino: 'Bloco A',
+        valor: 10,
+        nome: 'Mariana Lima',
+        avaliacao: 4.9,
+        bloco: 'B',
+        apto: '204'
+      },
+      {
+        data: '16/10',
+        hora: '17:00',
+        origem: 'TJRN',
+        destino: 'Res. Jardim Satélite',
+        valor: 10,
+        nome: 'Fred Silva',
+        avaliacao: 4.9,
+        bloco: 'B',
+        apto: '204'
+      }
+    ];
+  }
+}
